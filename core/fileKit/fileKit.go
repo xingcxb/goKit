4dd931a9932091966a9769c63f12b6fd7759b9de
xingcxb <xingcxb@hotmail.com>
2023-04-27 11:03:59 +0800

    fix(coreKit): 1. 新增获取指定文本文件内容的行数

--- core/fileKit/fileKit.go
+++ core/fileKit/fileKit.go
@@ -1,6 +1,38 @@
 package fileKit
 
-import "os"
+import (
+	"bufio"
+	"errors"
+	"os"
+)
+
+// GetFileTotalLines 获取文件总行数<br/>
+// 注意：<br/>
+//  1. 读取大文件时，会消耗大量内存
+//  2. 读取商业加密文本时行数上面会异常
+//  3. 空白行不会被计算
+//
+// @param filePath 文件路径
+func GetFileTotalLines(filePath string) (int, error) {
+	if filePath == "" {
+		return 0, errors.New("文件路径不能为空")
+	}
+	// 打开文件
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var lines int
+	for scanner.Scan() {
+		lines++
+	}
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+	return lines, nil
+}
 
 // FileDirSize 获取文件/文件夹下所有文件的大小
 // @param path 文件/文件夹路径
--- core/fileKit/file_test.go
+++ core/fileKit/file_test.go
@@ -7,4 +7,5 @@ import (
 
 func TestFileDirSize(t *testing.T) {
 	fmt.Println(FileDirSize("/Users/symbol/Downloads/Gyroflow-mac-universal.dmg"))
+	fmt.Println(GetFileTotalLines("/Users/symbol/Downloads/巨量.json"))
 }
