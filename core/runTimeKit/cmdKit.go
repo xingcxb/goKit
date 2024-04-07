d19205d913bf40398225103301235d567229894f
xingcxb <xingcxb@hotmail.com>
2024-04-07 15:08:51 +0800

    style:
    1. 优化命令执行说明
    2. 展示原始命令行返回信息

--- core/runTimeKit/cmdKit.go
+++ core/runTimeKit/cmdKit.go
@@ -6,15 +6,13 @@ import (
 
 // ExecuteCmd 执行shell命令
 /**
+ * 如要执行 `ls -l /home/user` 传值应该是 name=ls，arg="-l","/home/user"
  * @param shell 命令
  * @param parameter 参数
- * @return string 错误信息
+ * @return string,err 终端返回的信息，错误信息
  */
 func ExecuteCmd(shell string, parameter ...string) (string, error) {
 	cmd := exec.Command(shell, parameter...)
 	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", err
-	}
-	return string(output), nil
+	return string(output), err
 }
