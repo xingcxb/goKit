f4b5767d08862690c16389a9f2f0a473edf65fe1
xingcxb <xingcxb@hotmail.com>
2024-05-24 20:27:19 +0800

    feat
    1. add GetRandomValue
    
    fix
    
    1. GetVByIndex

--- core/arrayKit/mapKit.go
+++ core/arrayKit/mapKit.go
@@ -1,14 +1,30 @@
 package arrayKit
 
+import "github.com/xingcxb/goKit/core/randomKit"
+
 // GetVByIndex 通过下标取map中的值
 /*
  * @param m map[any]any 取值的map
  * @param index int 下标
  */
-func GetVByIndex(m map[any]any, index int) any {
-	_v := make([]any, len(m))
-	for k, v := range m {
-		_v[k.(int)] = v
+func GetVByIndex[K comparable, V any](m map[K]V, index int) any {
+	keys := make([]any, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
 	}
-	return _v[index]
+	return keys[index]
+}
+
+// GetRandomValue 随机获取map中的value
+/*
+ * @param m map[any]any
+ */
+func GetRandomValue[K comparable, V any](m map[K]V) V {
+	keys := make([]any, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
+	}
+	index := randomKit.RandomLong(len(keys))
+	randomKey := keys[index]
+	return m[randomKey]
 }
