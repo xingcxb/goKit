83594ca90ce8a55ff05fb59adc2a9a50c7c67cf7
xingcxb <xingcxb@hotmail.com>
2024-10-25 13:47:49 +0800

    [feat]
    1. ipKit 新增 IsPrivateIP 的判断方法

--- core/ipKit/ipKit.go
+++ core/ipKit/ipKit.go
@@ -3,6 +3,7 @@ package ipKit
 import (
 	"encoding/json"
 	"github.com/xingcxb/goKit/core/ipKit/ipService"
+	"net"
 )
 
 // GetIpAddressInfo 获取ip物理地址信息
@@ -20,4 +21,25 @@ func GetIpAddressInfo(ip string) (string, error) {
 		return "", err
 	}
 	return string(ipInfoJson), nil
+}
+
+// IsPrivateIP 判断是否为局域网 IP
+/*
+ * @param ip 需要判断的ip地址
+ * @return bool true 内网 false 局域网
+ */
+func IsPrivateIP(ip net.IP) bool {
+	privateIPBlocks := []string{
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+	}
+
+	for _, cidr := range privateIPBlocks {
+		_, subnet, _ := net.ParseCIDR(cidr)
+		if subnet.Contains(ip) {
+			return true
+		}
+	}
+	return false
 }
